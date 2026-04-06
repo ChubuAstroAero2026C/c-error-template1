5c2cc9ce551af121a92ccf6d358dac7020c54032
koosukeh <kosukeh@gs4e.chubu.ac.jp>
2026-04-06 19:27:59 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <studio.h>
 
 int main()
 {
     int a, b;
-    scanf(""%d"", &a);
-    scanf(""%d"", &b);
-    printf(""%d"", a+b);
+    scanf("%d", a)
+    scanf("%d", b)
+    printf("%d", a+b);
     return 0;
 }
